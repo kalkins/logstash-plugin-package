f13b8db0096529130dfb22cbada36caefb668b53
Sindre Stephansen <sindre@sindrestephansen.com>
2024-02-27 11:40:19 +0100

    Add docker job to install and package logstash plugins

---
+++ download-plugins.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+for file in ./plugins/*; do
+    echo "Installing plugins from $file"
+    plugins=""
+
+    while read plugin; do
+        echo "Installing $plugin"
+        logstash-plugin install $plugin
+        plugins="$plugins $plugin"
+    done <$file
+
+    version=$(logstash --version | awk '/^logstash/ { print $2 }')
+    outdir="out/$(basename $file .txt)"
+    outfile=$outdir/logstash-offline-plugins-$version.zip
+    mkdir -p $outdir
+
+    echo "Packaging plugins to $outfile"
+    logstash-plugin \
+        prepare-offline-pack \
+        --output "$outfile" \
+        $plugins
+done
